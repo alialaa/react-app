f3e729ce4719e89a0d782d1c48de70451035c9f6
Ali Alaa <hello@alialaa.com>
2020-01-27 11:23:49 +0200

    fix: dwdedwfef

--- release.config.js
+++ release.config.js
@@ -4,6 +4,14 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/github"
+    [
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" }
+        ]
+      }
+    ]
   ]
 };
