7003f89dc21d06f50c6efc0cdadd2dc1aa10a5ca
Ali Alaa <hello@alialaa.com>
2020-02-06 11:00:03 +0200

    feat(dewf): ewfe

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -3515,7 +3515,7 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  //   throw new Error("some error message");
+  throw new Error("some error message");
   const name = core.getInput("who-to-greet");
   console.log(`Hello ${name}`);
 
