6c2b52e772401556d4031f49b9ee32391124c667
Ali Alaa <hello@alialaa.com>
2020-02-06 11:17:09 +0200

    feat(dfds): fewf

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -3515,14 +3515,24 @@ const core = __webpack_require__(470);
 const github = __webpack_require__(469);
 
 try {
-  throw new Error("some error message");
+  //   throw new Error("some error message");
+
+  core.debug("Debug message");
+  core.warning("Warning message");
+  core.error("Error message");
+
   const name = core.getInput("who-to-greet");
+  core.setSecret(name);
   console.log(`Hello ${name}`);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("Logging github object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("HELLO", "hello");
 } catch (error) {
   core.setFailed(error.message);
 }
